611957f9f739f44821b21cc5236d2556860c0582
Victor <almenamvictor@gmail.com>
2024-10-20 19:22:39 +0200

    inalizacion de las clases Autor y Prestamo

--- src/Prestamo.java
+++ src/Prestamo.java
@@ -3,4 +3,45 @@ import java.util.Date;
 public class Prestamo {
     private int id_prestamo;
     private Date fechaFin, fechaInicio;
+
+    //Constructor vacio y general
+    public Prestamo() {
+    }
+
+    public Prestamo(int id_prestamo, Date fechaFin, Date fechaInicio) {
+        this.id_prestamo = id_prestamo;
+        this.fechaFin = fechaFin;
+        this.fechaInicio = fechaInicio;
+    }
+
+    //Getters y Setters
+
+    public int getId_prestamo() {
+        return id_prestamo;
+    }
+
+    public void setId_prestamo(int id_prestamo) {
+        this.id_prestamo = id_prestamo;
+    }
+
+    public Date getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(Date fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    public Date getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(Date fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    @Override
+    public String toString() {
+        return "Id del prestamo: " + id_prestamo + ", Fecha de inicio: " + fechaInicio + ", Fecha de fin: " + fechaFin;
+    }
 }
--- src/Usuario.java
+++ src/Usuario.java
@@ -1,4 +1,37 @@
 public class Usuario {
     private int id_usuario;
     private String nombre;
+    //Constructor vacio y general
+
+    public Usuario() {
+    }
+
+    public Usuario(int id_usuario, String nombre) {
+        this.id_usuario = id_usuario;
+        this.nombre = nombre;
+    }
+
+    //Getters y setters
+
+    public int getId_usuario() {
+        return id_usuario;
+    }
+
+    public void setId_usuario(int id_usuario) {
+        this.id_usuario = id_usuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    //To String
+    @Override
+    public String toString() {
+        return "Id usuario: " + id_usuario + ", Nombre: " + nombre;
+    }
 }
